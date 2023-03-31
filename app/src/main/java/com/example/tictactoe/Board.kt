1f9af2803eaa4d243c3f02cd53a509e9bdcaead3
sanao1006 <naosak1006@gmail.com>
2023-03-31 10:36:43 +0900

    ボードめっちゃ変えてる

--- app/src/main/java/com/example/tictactoe/Board.kt
+++ app/src/main/java/com/example/tictactoe/Board.kt
@@ -1,6 +1,7 @@
 package com.example.tictactoe
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -17,28 +18,23 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+
 @Composable
-fun Board(viewModel: PlayerViewModel = viewModel() ) {
+fun Board(viewModel: PlayerViewModel = viewModel()) {
     val boardState by viewModel.boardState
     Column {
-        for (i in 0 until 3) {
+        boardState.forEachIndexed { rIndex, row ->
             Row {
-                for (j in 0 until 3) {
-                    Box(
-                        modifier = Modifier
-                            .width(80.dp)
-                            .height(80.dp)
-                            .border(1.dp, Color.Black)
-                    ) {
-                        Button(
-                            onClick = { viewModel.onCellClicked(i, j) },
-                            modifier = Modifier.fillMaxSize(),
-                            colors = ButtonDefaults.buttonColors(
-                                backgroundColor = Color.Cyan
-                            )
-                        ) {
-
-
+                row.forEachIndexed { cIndex, cell ->
+                    Box(modifier = Modifier.clickable {
+//                        セルの状態書き換え
+//                        プレイヤーの変更
+                    }) {
+                        when (cell) {
+//                          セルの状態に応じてアイコンを表示
+                            CellState.CIRCLE -> {}
+                            CellState.CROSS -> {}
+                            CellState.EMPTY -> {}
                         }
                     }
                 }
